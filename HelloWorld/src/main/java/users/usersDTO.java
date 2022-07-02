569d24f15599720c8e2163644883e2a0cb2e3d31
kimalbab <kimalbab@gmail.com>
2022-07-02 17:04:43 +0900

    Tutorial_Test

---
+++ HelloWorld/src/main/java/users/usersDTO.java
@@ -0,0 +1,21 @@
+package users;
+
+public class usersDTO {
+	
+	String userID;
+	String userPassword;
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+	
+}
