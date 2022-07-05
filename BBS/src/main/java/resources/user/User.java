ecb2d765ca9421f80c5e35bf472a8bca2313b4e6
kimalbab <kimalbab@gmail.com>
2022-07-05 23:36:32 +0900

    Update
    http://localhost:8888/login.jsp  로 로그인해야 들어가진다!

---
+++ BBS/src/main/java/resources/user/User.java
@@ -0,0 +1,5 @@
+package resources.user;
+
+public class User {
+
+}
---
+++ BBS/src/main/java/resources/user/UserDAO.java
@@ -0,0 +1,16 @@
+package resources.user;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rset;
+	
+	public UserDao() {
+		String dbUrl = "jdbc:oracle:localhost"
+	}
+}
